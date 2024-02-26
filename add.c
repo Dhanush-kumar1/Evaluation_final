cba3707574029f9337f37a22a8ba431526dcf623
Dhanush Royce <157344739+Dhanush-kumar1@users.noreply.github.com>
2024-02-26 12:29:39 +0530

    Update add.c

--- add.c
+++ add.c
@@ -10,11 +10,7 @@ int main() {
     scanf("%d", &num2);
     
     sum = num1 + num2;
-    
+{
     printf("The sum of %d and %d is %d.", num1, num2, sum);
     
-    return 0; }
-}
-}}}
-}
-{}
+    return 0;
